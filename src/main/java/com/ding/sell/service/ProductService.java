a672ce54f8ac78de308e512feb4a28bd75c77c23
dj940212 <2902273280@qq.com>
2018-02-05 20:37:41 +0800

    完成商品Service层开发以及单元测试

---
+++ src/main/java/com/ding/sell/service/ProductService.java
@@ -0,0 +1,28 @@
+package com.ding.sell.service;
+import com.ding.sell.dataobject.ProductInfo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface ProductService {
+
+    /**
+     * 查询所有在架商品
+     */
+    ProductInfo findOne(String productId);
+
+    List<ProductInfo> findUpAll();
+
+    Page<ProductInfo> findAll(Pageable pageable);
+
+    ProductInfo save(ProductInfo productInfo);
+
+    /**
+     * 加库存
+     */
+
+    /**
+     * 减库存
+     */
+}
---
+++ src/main/java/com/ding/sell/service/impl/ProductServiceImpl.java
@@ -0,0 +1,38 @@
+package com.ding.sell.service.impl;
+
+import com.ding.sell.dataobject.ProductInfo;
+import com.ding.sell.enums.ProductStatusEnum;
+import com.ding.sell.repository.ProductInfoRepository;
+import com.ding.sell.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+    @Autowired
+    private ProductInfoRepository repository;
+
+    @Override
+    public ProductInfo findOne(String productId) {
+        return repository.findOne(productId);
+    }
+
+    @Override
+    public List<ProductInfo> findUpAll() {
+        return repository.findByProductStatus(ProductStatusEnum.UP.getCode());
+    }
+
+    @Override
+    public Page<ProductInfo> findAll(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @Override
+    public ProductInfo save(ProductInfo productInfo) {
+        return repository.save(productInfo);
+    }
+}
