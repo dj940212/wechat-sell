cfd4391554751ba7144d7f865d1eb30441727880
dj940212 <2902273280@qq.com>
2018-02-08 00:55:51 +0800

    [接口] 完成创建订单接口

---
+++ src/main/java/com/ding/sell/controller/BuyerOrderController.java
@@ -0,0 +1,52 @@
+package com.ding.sell.controller;
+
+import com.ding.sell.VO.ResultVO;
+import com.ding.sell.converter.OrderForm2OrderDTOConverter;
+import com.ding.sell.dto.OrderDTO;
+import com.ding.sell.enums.ResultEnum;
+import com.ding.sell.exception.SellException;
+import com.ding.sell.form.OrderForm;
+import com.ding.sell.service.OrderService;
+import com.ding.sell.utils.ResultVOUtil;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/buyer/product")
+@Slf4j
+public class BuyerOrderController {
+    @Autowired
+    private OrderService orderService;
+
+    /*创建订单*/
+    @PostMapping("/create")
+    public ResultVO<Map<String, String>> create(@Valid OrderForm orderForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error("[创建订单] 参数不正确, orderForm={}", orderForm);
+            throw new SellException(ResultEnum.PARAM_ERROR.getCode(), bindingResult.getFieldError().getDefaultMessage());
+        }
+
+        OrderDTO orderDTO = OrderForm2OrderDTOConverter.convert(orderForm);
+        if (CollectionUtils.isEmpty(orderDTO.getOrderDetailList())){
+            log.error("[创建订单] 购物车不能为空");
+            throw new SellException(ResultEnum.CART_EMPTY);
+        }
+
+        OrderDTO createResult = orderService.create(orderDTO);
+
+        Map<String, String> map = new HashMap<>();
+        map.put("orderId", createResult.getOrderId());
+
+        return ResultVOUtil.success(map);
+    }
+
+}
--- src/main/java/com/ding/sell/enums/ResultEnum.java
+++ src/main/java/com/ding/sell/enums/ResultEnum.java
@@ -4,13 +4,17 @@ import lombok.Getter;
 
 @Getter
 public enum ResultEnum {
+    PARAM_ERROR(1, "参数不正确"),
+
+
     PRODUCT_NOT_EXIST(10, "商品不存在"),
     PRODUCT_STOCK_ERROR(11, "商品库存不正确"),
     ORDER_NOT_EXIST(12,"订单不存在"),
-    ORDERDETAIL_NOT_EXIST(12,"订单详情不存在"),
-    ORDER_STATUS_ERROR(13,"订单状态错误"),
-    ORDER_UPDATE_FAIL(14,"订单更新失败"),
-    ORDER_DETAIL_EMPTY(15,"订单详情为空");
+    ORDERDETAIL_NOT_EXIST(13,"订单详情不存在"),
+    ORDER_STATUS_ERROR(14,"订单状态错误"),
+    ORDER_UPDATE_FAIL(15,"订单更新失败"),
+    ORDER_DETAIL_EMPTY(16,"订单详情为空"),
+    CART_EMPTY(17, "购物车为空");
 
     private Integer code;
     private String message;
